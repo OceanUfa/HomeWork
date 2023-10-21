29a769861a4f8214bc1c8c7a14dab62dee8bad33
vadim <1234@yrtdsf.ru>
2023-10-21 14:18:47 +0300

    Work2

---
+++ Work2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int m, int n)
+{
+  if (m == n) return n;
+  else return SumOfElements(m + 1, n) + m;
+}
+
+int m = Prompt("Input M: ");
+int n = Prompt("Input N: ");
+
+Console.WriteLine(SumOfElements(m, n));
